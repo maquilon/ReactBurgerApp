823d65ce1af1d9a24bb1bd66e0c3a9896cdb2231
Humberto Maquilon <humberto@viomedia.com>
2018-05-31 21:29:48 -0400

    Added BurgerIngredient

--- burger-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -20,27 +20,31 @@ class BurgerIngredient extends Component {
                     </div>
                 );
                 break;
+
             case ('meat'):
-                ingredient = <div className={classes.Meat}></div>
+                ingredient = <div className={classes.Meat}></div>;
                 break;
-            case ('cheease'):
-                ingredient = <div className={classes.Cheese}></div>
+
+            case ('cheese'):
+                ingredient = <div className={classes.Cheese}></div>;
                 break;
-            case ('salad'):
-                ingredient = <div className={classes.Salad}></div>
-                break;
+
             case ('bacon'):
-                ingredient = <div className={classes.Bacon}></div>
+                ingredient = <div className={classes.Bacon}></div>;
+                break;
+
+            case ('salad'):
+                ingredient = <div className={classes.Salad}></div>;
                 break;
 
             default:
-                ingredient = null
+                ingredient = null;
         }
         return ingredient;
     }
 }
 
-BurgerIngredient.prototype = {
+BurgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
 }
 
